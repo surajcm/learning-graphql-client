200d2be749a0575929ee942f9e3ec85bcda219cf
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-06-22 09:44:42 +0530

    first commit

---
+++ src/main/java/com/suraj/learninggraphqlclient/LearningGraphqlClientApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.learninggraphqlclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGraphqlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGraphqlClientApplication.class, args);
+	}
+
+}

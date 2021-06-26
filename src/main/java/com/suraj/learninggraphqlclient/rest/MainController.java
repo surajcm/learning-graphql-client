f6bf1cdc7670004e42381572fb83cd96a8b221de
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-06-26 11:40:31 +0530

    spring boot 2.5.2 endpoint update

--- src/main/java/com/suraj/learninggraphqlclient/rest/MainController.java
+++ src/main/java/com/suraj/learninggraphqlclient/rest/MainController.java
@@ -12,8 +12,9 @@ public class MainController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }

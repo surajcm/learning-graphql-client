ec78e691a2b0b4446e6826823ea7d5c1d043b41a
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-07-09 07:50:14 +0530

    Implemented connectivity to Rick and Monty api

--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
 <h1 align="center">
     learning-graphql-client
 </h1>
-<p align="center">
+
     The intention of this project is to publish a REST service by consuming some values from a public graphql api. This project is intend to use a REST client or any other library to establish the connectivity.
-</p>
+
+For downloading the latest schema, please use the following command
+```shell
+./gradlew downloadApolloSchema \
+  --endpoint="https://rickandmortyapi.com/graphql" \
+  --schema="src/main/graphql/com/rick/morty/schema.json"
+
+```
+
+Generated sources will be present inside `build/sources` directory, we may need to add that to sources.
+
+
+
+#### Swagger UI
+Please check at http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config
+
+#### Reference
+https://www.apollographql.com/docs/android/
+https://moonhighway.com/public-graphql-apis/
+https://graphql.org/code/#java-kotlin
+https://github.com/k0kubun/graphql-query-builder
+https://github.com/leangen/graphql-spqr
+
--- src/main/java/com/suraj/learninggraphqlclient/rest/MainController.java
+++ src/main/java/com/suraj/learninggraphqlclient/rest/MainController.java
@@ -1,20 +1,67 @@
 package com.suraj.learninggraphqlclient.rest;
 
-import com.suraj.learninggraphqlclient.rest.model.Greeting;
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rick.morty.EpisodesByIdsQuery;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
+@RequestMapping("/rick")
 public class MainController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+    private String responseHolder = null;
 
-    @GetMapping("/")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping(name = "/morty/", produces = "application/json")
+    public ResponseEntity<Object> episodeById(@RequestParam(value = "id", defaultValue = "1") String id) {
+        ApolloClient apolloClient = ApolloClient.builder()
+                .serverUrl("https://rickandmortyapi.com/graphql")
+                .build();
+        logger.info("Started querying....");
+        apolloClient.query(new com.rick.morty.EpisodesByIdsQuery(List.of("1")))
+                .enqueue(new ApolloCall.Callback<>() {
 
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+                    @Override
+                    public void onResponse(@NotNull Response<EpisodesByIdsQuery.Data> response) {
+                        populateResponseHolder(response.getData().episodesByIds().get(0));
+                        logger.info("Apollo :first id is: " + response.getData().episodesByIds().get(0).name());
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull ApolloException e) {
+                        logger.error("Apollo : Error", e);
+                    }
+                });
+        logger.info("Done querying....");
+        return new ResponseEntity<>(responseHolder, HttpStatus.OK);
+    }
+
+    private void populateResponseHolder(EpisodesByIdsQuery.EpisodesById episodesById) {
+        String resp;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            resp = mapper.writeValueAsString(episodesById);
+            logger.info("resp is :"+resp);
+        } catch (IOException ex) {
+            resp = "ERROR";
+            logger.error("error parsing to json : " + ex.getMessage());
+        }
+        responseHolder = resp;
     }
 }

593b745042f8f36b1e40148814f3feadc846eea4
Suraj Muraleedharan <surajcm@gmail.com>
2022-04-01 08:18:14 +0530

    Upgrading to java 18, gradle 7.4.1

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-16.0.1.jdk/Contents/Home"
-
-#export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64/"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-18.jdk/Contents/Home"
 export JAVA_OPTS="-Xms512m -Xmx512m"
 exec ./gradlew "$@"
